1bf7b382fe518f9602517c8a9a91a74770355461
Hermes Rodriguez <HappyBT0731@hotmail.com>
2026-07-30 08:59:10 -0400

    Agregar acceso con código

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+const botonVerificar = document.getElementById("verificar");
+const inputCodigo = document.getElementById("codigo");
+const acceso = document.getElementById("acceso");
+const contenido = document.getElementById("contenido");
+const error = document.getElementById("error");
+
+botonVerificar.addEventListener("click", function () {
+
+    if (inputCodigo.value === "3107") {
+
+        acceso.style.display = "none";
+        contenido.style.display = "block";
+
+    } else {
+
+        error.textContent = "Esa no parece ser la clave.";
+
+        inputCodigo.value = "";
+        inputCodigo.focus();
+
+    }
+
+});
+
+inputCodigo.addEventListener("keypress", function (e) {
+
+    if (e.key === "Enter") {
+
+        botonVerificar.click();
+
+    }
+
+});
+
 document.getElementById("continuar").addEventListener("click", function () {
+
     window.location.href = "carta.html";
+
 });
